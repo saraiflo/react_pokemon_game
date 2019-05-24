76d67bc620ce04757c5fe48906c40831b33293a5
Sarai <saraiflores@gmail.com>
2019-05-24 01:23:50 -0400

    adding all code for pokemon game

---
+++ src/Pokecard.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./Pokecard.css";
+
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+let padToThree = number => (number <= 999 ? `00${number}`.slice(-3) : number);
+
+class Pokecard extends Component {
+  render() {
+    const { id, name, type, exp } = this.props;
+    const imgSrc = `${POKE_API}${padToThree(this.props.id)}.png `;
+    return (
+      <div className="Pokecard">
+        <h1 className="Pokecard-title">{name}</h1>
+        <div>
+          <img className="Pokecard-image" src={imgSrc} alt={name} />
+        </div>
+        <div className="Pokecard-data">Type={type}</div>
+        <div className="Pokecard-data">Experience={exp}</div>
+      </div>
+    );
+  }
+}
+
+export default Pokecard;
